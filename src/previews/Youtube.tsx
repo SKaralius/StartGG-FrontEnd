565cde5c082a82a5554ca60d29b86ee5e5011bcd
SKaralius <k.simonas.k@gmail.com>
2020-09-16 17:10:24 +0300

    Youtube bar and formating.

--- src/previews/Youtube.tsx
+++ src/previews/Youtube.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import useGetData from "../util/useGetData";
 import { getYoutubeTrending } from "../util/api";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import YoutubeBar from "./YoutubeBar";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 function Youtube({
   handleLoadMore,
@@ -10,10 +12,35 @@ function Youtube({
   handleLoadMore: () => void;
   expand: boolean;
 }) {
+  dayjs.extend(relativeTime);
   const posts: any = useGetData(getYoutubeTrending);
+  //PT1M27S
+  function formatDuration(duration: string) {
+    let formatedDuration: string = "";
+    const durationArray = duration.split("");
+
+    for (let i = durationArray.length; i >= 0; i--) {
+      if (durationArray[i] === "S") {
+        i--;
+        formatedDuration += durationArray[i];
+        i--;
+        formatedDuration += durationArray[i];
+        formatedDuration += ":";
+      }
+      if (durationArray[i] === "M") {
+        i--;
+        formatedDuration += durationArray[i];
+        i--;
+        if (typeof durationArray[i] === "number")
+          formatedDuration += durationArray[i];
+      }
+    }
+    formatedDuration = formatedDuration.split("").reverse().join("");
+    return formatedDuration;
+  }
   interface post {
     id: string;
-    contentDetails: { duration: Number };
+    contentDetails: { duration: string };
     snippet: {
       channelTitle: string;
       publishedAt: Date;
@@ -25,25 +52,33 @@ function Youtube({
     const postCollection = posts.map((post: post) => {
       return (
         <li key={post.id} className="youtube-post">
-          <a
-            href={`https://youtube.com/watch?v=${post.id}`}
-            className="youtube-link-img"
-          >
-            <img
-              src={post.snippet.thumbnails.default.url}
-              alt={post.snippet.title}
-            />
-          </a>
+          <div className="youtube-img-container">
+            <a
+              href={`https://youtube.com/watch?v=${post.id}`}
+              className="youtube-link-img"
+            >
+              <img
+                src={post.snippet.thumbnails.default.url}
+                alt={post.snippet.title}
+              />
+            </a>
+            <div className="youtube-duration">
+              {formatDuration(post.contentDetails.duration)}
+            </div>
+          </div>
           <strong className="youtube-title">{post.snippet.title}</strong>
-          <span>{post.contentDetails.duration}</span>
-          <span>{post.snippet.channelTitle}</span>
-          <span>{post.snippet.publishedAt}</span>
+          <span className="youtube-channel-title">
+            {post.snippet.channelTitle}
+          </span>
+          <span className="youtube-date">
+            {dayjs(post.snippet.publishedAt).fromNow()}
+          </span>
         </li>
       );
     });
     return (
       <div className="youtube">
-        <FontAwesomeIcon size="2x" icon={["fab", "youtube"]} color="#FF0000" />
+        <YoutubeBar />
         <ul className="preview youtube-posts">{postCollection}</ul>
         <div className="youtube-shadow">
           <button className="youtube-more" onClick={handleLoadMore}>
