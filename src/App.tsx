ad782713fbd888277a9b0d8cd0884ba6c47b880e
SKaralius <k.simonas.k@gmail.com>
2020-09-17 12:26:00 +0300

    Sticky state for selected view.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import PreviewContainer from "./previews/PreviewsContainer";
 import "./scss/App.scss";
 import Search from "./previews/Search";
@@ -6,9 +6,10 @@ import { WEBSITES } from "./util/constants";
 import { getRedditFrontPage } from "./util/api";
 import { getYoutubeTrending } from "./util/api";
 import useGetData from "./util/useGetData";
+import useStickyState from "./util/useStickyState";
 
 function App() {
-  const [isSelected, setIsSelected] = useState<boolean | string>(false);
+  const [isSelected, setIsSelected] = useStickyState(false, "website");
   const handleSetIsSelected = (website: string | boolean) => {
     setIsSelected(website);
   };
