d44bac68678c34c7768b521329c0d15b0a2a246f
SKaralius <k.simonas.k@gmail.com>
2020-09-21 16:31:56 +0300

    Api for production.

--- src/util/api.js
+++ src/util/api.js
@@ -1,8 +1,10 @@
 const httpService = {};
+let address = `http://${process.env.REACT_APP_SERVER_ADDRESS}:${process.env.REACT_APP_SERVER_PORT}`;
+if (process.env.NODE_ENV === "production") {
+  address = process.env.REACT_APP_PRODUCTION_SERVER_ADDRESS;
+}
 httpService.get = (route) => {
-  return window.fetch(
-    `http://${process.env.REACT_APP_SERVER_ADDRESS}:${process.env.REACT_APP_SERVER_PORT}/${route}`
-  );
+  return window.fetch(`${address}/${route}`);
 };
 
 export const getRedditFrontPage = (pageNumber) =>
